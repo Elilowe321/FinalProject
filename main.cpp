67a628e7476d8a5449908defbb6231634612aa30
elilo <elijah.lowe@ufl.edu>
2023-04-23 19:20:43 -0400

    Make page with text boxes for car make and zip code of user

--- main.cpp
+++ main.cpp
@@ -5,21 +5,136 @@
 
 using namespace std;
 
+sf::RectangleShape textBox() {
+
+    // Set up the rectangle
+    sf::RectangleShape rectangle(sf::Vector2f(200, 50));
+    rectangle.setFillColor(sf::Color::White);
+    rectangle.setOutlineThickness(2);
+    rectangle.setOutlineColor(sf::Color::Black);
+
+    return rectangle;
+}
+
 int main() {
 
-    sf::Window window(sf::VideoMode(800, 600), "My window");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    //window.setFramerateLimit(60);
 
-    while(window.isOpen()) {
+    //==========Set Up text box, text, and inputs for car make==========
+    sf::RectangleShape carMakeRectangle = textBox();
+    std::string carMakeInput;
+
+    sf::Font font;
+    font.loadFromFile("arial.ttf");
+
+    //Hold text for car make
+    sf::Text carMakeText;
+    carMakeText.setFont(font);
+    carMakeText.setCharacterSize(24);
+    carMakeText.setFillColor(sf::Color::Black);
+    carMakeText.setString("");
+
+    //Car make Heading
+    sf::Text makeHeading("Car Make", font, 36);
+    makeHeading.setFillColor(sf::Color::Blue);
+
+
+    //==========Set up text, box, text, and inputs for zip code==========
+    sf::RectangleShape zipCodeRectangle = textBox();
+    std::string zipCodeInput;
+
+    //Hold text for zip code
+    sf::Text zipCodeText;
+    zipCodeText.setFont(font);
+    zipCodeText.setCharacterSize(24);
+    zipCodeText.setFillColor(sf::Color::Black);
+    zipCodeText.setString("");
+
+    //Zip code Heading
+    sf::Text zipCodeHeading("Zip Code", font, 36);
+    zipCodeHeading.setFillColor(sf::Color::Blue);
+
+
+
+    while (window.isOpen()) {
 
         sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
+
+        while (window.pollEvent(event)) {
+
+            if (event.type == sf::Event::Closed) { //Exit program
                 window.close();
+            }
+
+            //User is typing in the car make text box
+            if (event.type == sf::Event::TextEntered && carMakeRectangle.getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
+
+                if (event.text.unicode < 128 && event.text.unicode != '\b') { //Get input for car make
+
+                    carMakeText.setString(carMakeText.getString() + static_cast<char>(event.text.unicode));
+                    carMakeInput = carMakeText.getString();
+                }
+                else if (event.text.unicode == '\b') { // Handle backspace
+
+                    std::string str = carMakeText.getString();
+                    if (str.size() > 0) {
+                        str.pop_back();
+                        carMakeText.setString(str);
+                        carMakeInput = carMakeText.getString();
+                    }
+                }
+            }
+
+            //User is typing in the zip code text box
+            if (event.type == sf::Event::TextEntered && zipCodeRectangle.getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y)) {
+
+                if (event.text.unicode < 128 && event.text.unicode != '\b') { //Get input for zip code
+
+                    zipCodeText.setString(zipCodeText.getString() + static_cast<char>(event.text.unicode));
+
+                    zipCodeInput = zipCodeText.getString();
+                }
+                else if (event.text.unicode == '\b') { // Handle backspace
+
+                    std::string str = zipCodeText.getString();
+                    if (str.size() > 0) {
+                        str.pop_back();
+                        zipCodeText.setString(str);
+                        zipCodeInput = zipCodeText.getString();
+                    }
+                }
+            }
         }
+
+        window.clear(sf::Color::White);
+
+
+        //Draw All top left input(make of car)
+        carMakeRectangle.setPosition(50,50);
+        makeHeading.setPosition(carMakeRectangle.getPosition().x, carMakeRectangle.getPosition().y - 50);
+        carMakeText.setPosition(carMakeRectangle.getPosition().x + 10, carMakeRectangle.getPosition().y + 10);
+        window.draw(carMakeRectangle);
+        window.draw(makeHeading);
+        window.draw(carMakeText);
+
+        //Draw middle inputs (zip code)
+        zipCodeRectangle.setPosition(400,50);
+        zipCodeHeading.setPosition(zipCodeRectangle.getPosition().x, zipCodeRectangle.getPosition().y - 50);
+        zipCodeText.setPosition(zipCodeRectangle.getPosition().x + 10, zipCodeRectangle.getPosition().y + 10);
+        window.draw(zipCodeRectangle);
+        window.draw(zipCodeHeading);
+        window.draw(zipCodeText);
+
+
+        window.display();
     }
 
-    std::cout << "Hello, World!" << std::endl;
+
+    std::cout << "Text = " << carMakeInput << std::endl;
+
+    int actualZipCode = stoi(zipCodeInput);
+    std::cout << "ZipCode = " << actualZipCode << std::endl;
 
     return 0;
 }
